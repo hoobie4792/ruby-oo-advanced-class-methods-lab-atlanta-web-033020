91cc2baf3a27997d41f6202212179a0b12104117
Brandon Brown <brandonbrown4792@gmail.com>
2020-04-01 00:19:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,8 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name
+    song = find_by_name(name)
+    if song
       song
     else
       self.create_by_name(name)
